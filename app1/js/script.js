99b95629c910a28ca7460dfc0115280b0cdf779d
Дмитрий Варганов <varganovda@gmail.com>
2021-03-04 18:07:33 +0300

    feat(js): updated an algorithm of the app

--- app1/js/script.js
+++ app1/js/script.js
@@ -8,8 +8,27 @@ const personalMovieDB = {
   private: false,
 };
 
-const lastViewedFilm = prompt('Один из последних просмотренных фильмов', '');
-const lastViewedFilmMark = prompt('На сколько его оцените?', '');
+for(let i = 0; i < 2; i++) {
+  const lastViewedFilm = prompt('Один из последних просмотренных фильмов', ''),
+        lastViewedFilmMark = prompt('На сколько его оцените?', '');
 
-personalMovieDB.movies[lastViewedFilm] = lastViewedFilmMark;
+  if (!!lastViewedFilm && !!lastViewedFilmMark && lastViewedFilm.length < 50) {
+    personalMovieDB.movies[lastViewedFilm] = lastViewedFilmMark;
+    console.log('done');
+  } else {
+    console.log('error');
+    i--;
+  }
+}
+
+if (!!personalMovieDB.count && personalMovieDB.count < 10) {
+  console.log('Вы посмотрели слишком мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+  console.log('Вы киноман');
+} else {
+  console.log('Произошла ошибка');
+}
+
 console.log(personalMovieDB);
