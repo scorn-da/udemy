0ad6f09a584280b8c9871a2152d3c4630ddf50d5
Дмитрий Варганов <varganovda@gmail.com>
2021-04-05 12:51:49 +0300

    feat(scss, js): added tabs classes and a script to use tabs

---
+++ app2/src/js/main.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const tabsContainer = document.querySelector('.tabcontainer');
+  const tabs = tabsContainer.querySelectorAll('.tabheader__item');
+  const tabsContent = tabsContainer.querySelectorAll('.tabcontent');
+  const tabsParent = tabsContainer.querySelector('.tabheader__items');
+
+
+  function hideTabContent () {
+    tabsContent.forEach(item => {
+      item.classList.add('hide');
+      item.classList.remove('show', 'fade');
+    });
+
+    tabs.forEach(tab => {
+      tab.classList.remove('tabheader__item_active');
+    });
+  }
+  
+  function showTabContent(i = 0) {
+    tabsContent[i].classList.add('show', 'fade');
+    tabsContent[i].classList.remove('hide');
+    tabs[i].classList.add('tabheader__item_active');
+  }
+
+  hideTabContent();
+  showTabContent(0);
+
+  tabsParent.addEventListener('click', (evt) => {
+    const target = evt.target;
+
+    if (target && target.classList.contains('tabheader__item')) {
+      tabs.forEach((item, i) => {
+        if (target == item) {
+          hideTabContent();
+          showTabContent(i);
+        }
+      });
+    }
+  });
+});
