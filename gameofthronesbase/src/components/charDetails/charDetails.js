f0eb88d89abdb7b62bd49dfc781eb257abe3d54f
Дмитрий Варганов <varganovda@gmail.com>
2021-07-13 02:29:43 +0300

    style(components): updated the charDetails component code style

--- gameofthronesbase/src/components/charDetails/charDetails.js
+++ gameofthronesbase/src/components/charDetails/charDetails.js
@@ -4,23 +4,23 @@ export default class CharDetails extends Component {
 
     render() {
         return (
-            <div className="char-details rounded">
+            <div className='char-details rounded'>
                 <h4>John Snow</h4>
-                <ul className="list-group list-group-flush">
-                    <li className="list-group-item d-flex justify-content-between">
-                        <span className="term">Gender</span>
+                <ul className='list-group list-group-flush'>
+                    <li className='list-group-item d-flex justify-content-between'>
+                        <span className='term'>Gender</span>
                         <span>male</span>
                     </li>
-                    <li className="list-group-item d-flex justify-content-between">
-                        <span className="term">Born</span>
+                    <li className='list-group-item d-flex justify-content-between'>
+                        <span className='term'>Born</span>
                         <span>1783</span>
                     </li>
-                    <li className="list-group-item d-flex justify-content-between">
-                        <span className="term">Died</span>
+                    <li className='list-group-item d-flex justify-content-between'>
+                        <span className='term'>Died</span>
                         <span>1820</span>
                     </li>
-                    <li className="list-group-item d-flex justify-content-between">
-                        <span className="term">Culture</span>
+                    <li className='list-group-item d-flex justify-content-between'>
+                        <span className='term'>Culture</span>
                         <span>First</span>
                     </li>
                 </ul>
