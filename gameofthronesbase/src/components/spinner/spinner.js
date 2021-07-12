53b603718462ca4f98483af6e1171e9294c3d849
Дмитрий Варганов <varganovda@gmail.com>
2021-07-13 02:25:15 +0300

    feat(components): added a spinner component in the project

---
+++ gameofthronesbase/src/components/spinner/spinner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './spinner.css';
+
+const Spinner = () => {
+  return (
+    <div className='loadingio-spinner-eclipse-vww9d9jb7z9'>
+      <div className='ldio-f97ji03jhc'>
+        <div />
+      </div>
+    </div>
+  )
+};
+
+export default Spinner;
