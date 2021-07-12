3b2c6d1229e0af0db39f7a7175a180c0fd28bdaa
Дмитрий Варганов <varganovda@gmail.com>
2021-07-13 02:35:37 +0300

    feat(components): added a state of the randomChar component

--- gameofthronesbase/src/components/app/app.js
+++ gameofthronesbase/src/components/app/app.js
@@ -1,34 +1,45 @@
-import React from 'react';
-import {Col, Row, Container} from 'reactstrap';
+import React, { useState } from 'react';
+import { Col, Row, Container } from 'reactstrap';
 import Header from '../header';
 import RandomChar from '../randomChar';
 import ItemList from '../itemList';
 import CharDetails from '../charDetails';
 
-
 const App = () => {
-    return (
-        <> 
-            <Container>
-                <Header />
-            </Container>
-            <Container>
-                <Row>
-                    <Col lg={{size: 5, offset: 0}}>
-                        <RandomChar/>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col md='6'>
-                        <ItemList />
-                    </Col>
-                    <Col md='6'>
-                        <CharDetails />
-                    </Col>
-                </Row>
-            </Container>
-        </>
-    );
+  const [isRandomCharHidden, setIsRandomCharHidden] = useState(false);
+
+  const toggleRandomCharState = () => {
+    setIsRandomCharHidden(!isRandomCharHidden);
+  };
+
+  return (
+    <>
+      <Container>
+        <Header/>
+      </Container>
+      <Container>
+        <Row>
+          <Col lg={{size: 5, offset: 0}}>
+            <RandomChar isRandomCharHidden={isRandomCharHidden} />
+            <button
+              className='btn btn-md btn-primary mb-4'
+              onClick={toggleRandomCharState}
+            >
+              Toggle character widget state
+            </button>
+          </Col>
+        </Row>
+        <Row>
+          <Col md='6'>
+            <ItemList/>
+          </Col>
+          <Col md='6'>
+            <CharDetails/>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 };
 
 export default App;
