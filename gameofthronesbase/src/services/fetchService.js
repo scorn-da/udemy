4e2800cdf42830f76dbef60785605feec463508e
Дмитрий Варганов <varganovda@gmail.com>
2021-07-13 02:24:42 +0300

    feat(services): added functions to get more items by api

--- gameofthronesbase/src/services/fetchService.js
+++ gameofthronesbase/src/services/fetchService.js
@@ -1,26 +1,75 @@
-import react, { Component } from 'react';
-
-export class FetchService extends Component {
-  constructor(props) {
-    super(props);
+export default class FetchService {
+  constructor() {
     this._apiBase = 'https://anapioficeandfire.com/api';
   }
 
   async getResources(url) {
     const res = await fetch(`${this._apiBase}${url}`);
 
-    if(!res.ok) {
+    if (!res.ok) {
       throw new Error(`Couldn't fetch ${url}, status: ${res.status}`);
     }
 
     return await res.json();
   }
 
-  getAllItems() {
-    return this.getResources('/characters?page=5&pageSize=10');
+  async getAllCharacters() {
+    const res = await this.getResources('/characters?page=5&pageSize=10');
+    return res.map(this._updateCharacter);
   }
 
-  getItemById(id) {
-    return this.getResources(`/characters/${id}`);
+  async getCharacterById(id) {
+    const char = await this.getResources(`/characters/${id}`);
+    return this._updateCharacter(char);
+  }
+
+  getAllHousesInfo() {
+    return this.getResources(`/houses/`);
+  }
+
+  getHouseInfo(id) {
+    return this.getResources(`/houses/${id}/`);
+  }
+
+  getAllBooks() {
+    return this.getResources(`/books/`);
+  }
+
+  getBook(id) {
+    return this.getResources(`/books/${id}`);
+  }
+
+  _getItemInfo(item) {
+
+  }
+
+  _updateCharacter(char) {
+    return {
+      name: char.name || 'no data :(',
+      gender: char.gender,
+      born: char.born,
+      died: char.died,
+      culture: char.culture,
+    }
+  }
+
+  _updateHouse(house) {
+    return {
+      name: house.name,
+      region: house.region,
+      words: house.words,
+      titles: house.titles,
+      overlord: house.overlord,
+      ancestralWeapons: house.ancestralWeapons,
+    }
+  }
+
+  _updateBook(book) {
+    return {
+      name: book.name,
+      numberOfPages: book.numberOfPages,
+      publisher: book.publisher,
+      released: book.released,
+    }
   }
 }
