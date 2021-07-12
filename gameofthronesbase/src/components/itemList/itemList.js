87d81bb75c98a161f136497ea728819f3060f357
Дмитрий Варганов <varganovda@gmail.com>
2021-07-13 02:30:23 +0300

    style(components): updated the itemList component code style

--- gameofthronesbase/src/components/itemList/itemList.js
+++ gameofthronesbase/src/components/itemList/itemList.js
@@ -4,14 +4,14 @@ export default class ItemList extends Component {
 
     render() {
         return (
-            <ul className="item-list list-group">
-                <li className="list-group-item">
+            <ul className='item-list list-group'>
+                <li className='list-group-item'>
                     John Snow
                 </li>
-                <li className="list-group-item">
+                <li className='list-group-item'>
                     Brandon Stark
                 </li>
-                <li className="list-group-item">
+                <li className='list-group-item'>
                     Geremy
                 </li>
             </ul>
